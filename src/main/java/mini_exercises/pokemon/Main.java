16accc970cf4fdf6dacb106a598dc7a5be6ac806
aminaaso <aaminaa.sohail@gmail.com>
2025-04-25 10:06:14 +0500

    Pokemon Main Function

---
+++ src/main/java/mini_exercises/pokemon/Main.java
@@ -0,0 +1,16 @@
+package mini_exercises.pokemon;
+
+public class Main {
+    public static void main(String[] args) {
+        //create objects
+        //Class objName = new Class();
+        Pokemon player1 = new Pokemon("Fatima", 4);
+        //obj.name
+        //obj.level
+
+        //obj.methodname()
+        player1.attack();
+
+        System.out.println(player1.level);
+    }
+}
